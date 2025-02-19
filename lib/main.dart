597b8786c4136b6eb6d137a8c23a647d4cbf700e
abigailsheldon <abigail.l.sheldon@gmail.com>
2025-02-19 18:42:38 -0500

    Adds win/lose functionality with a popup alert if user wins or loses

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
     Timer? hungerTimer;
     int happinessLevel = 50;
     int hungerLevel = 50;
+    bool gameOver = false;
 
   @override
   void initState() {
@@ -26,18 +27,79 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
   void _startHungerTimer() {
     hungerTimer = Timer.periodic(Duration(seconds: 30), (timer) 
     {
+      if (!gameOver){
         setState(() {
           hungerLevel = (hungerLevel + 5).clamp(0, 100);
           _updateHappiness();
+          _checkGameOver();
         });
+      }
     });
   }
+
+  // Function to reset the game
+  void _resetGame() {
+    setState(() {
+      happinessLevel = 50;
+      hungerLevel = 50;
+      gameOver = false;
+      _startHungerTimer();
+    });
+  }
+
+  // Function to check for win condition
+  void _checkWin() {
+    if (happinessLevel > 80) {
+      Future.delayed(Duration(minutes: 2), () {
+        if (happinessLevel > 70) {
+          _showDialog("You Win!", "Your pet is happy and well cared for!", reset:true);
+        }
+      });
+    }
+  }
+
+  // Function to check game over condition
+  void _checkGameOver() {
+    if (hungerLevel == 100 && happinessLevel <= 10) {
+      gameOver = true;
+      hungerTimer?.cancel();
+      _showDialog("Game Over", "Your pet was unhappy and has starved to death...");
+    }
+  }
+
+
+  // Function to show dialogs
+  void _showDialog(String title, String message, {bool reset = false}) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                if (reset) {
+                  _resetGame();
+                }
+              },
+              child: Text("YAY!"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   
   // Function to increase happiness and update hunger when playing with the pet
   void _playWithPet() {
     setState(() {
       happinessLevel = (happinessLevel + 10).clamp(0, 100);
       _updateHunger();
+      _checkWin();
+      _checkGameOver();
     });
   }
   
@@ -46,6 +108,8 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
     setState(() {
       hungerLevel = (hungerLevel - 10).clamp(0, 100);
       _updateHappiness();
+      _checkWin();
+      _checkGameOver();
     });
   }
   
